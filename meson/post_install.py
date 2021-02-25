1a3ec59eb4b203ebfd65cbf80a7108fd5eccb1ae
Lain1447 <hana19.nf@protonmail.ch>
2021-02-25 16:05:55 +0000

    remove 2nd i18n

--- meson/post_install.py
+++ meson/post_install.py
@@ -5,6 +5,3 @@ schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'schemas')
 if not os.environ.get('DESTDIR'):
     print('Compiling schemas...')
     subprocess.call(['cpp-compile-schemas', schemadir])
-
-
-
