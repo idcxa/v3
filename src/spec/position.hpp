d6a16b73fea26e5e0a531245019966129bf9a868
sxphie <hana19.nf@protonmail.ch>
2021-02-25 17:24:31 +0000

    pieces struct + classes idea

--- src/spec/position.hpp
+++ src/spec/position.hpp
@@ -27,3 +27,35 @@ public:
     auto innerIterator (int x, int y);
     auto outerIterator (int x, int y);
 };
+
+typedef struct Pieces
+{   // Define piece values & such here
+
+    Bitboard positions;
+    bool attacked, pinned, checking;
+    const int value;
+    const char * moves;
+
+};
+
+/**
+- Another idea on how to implement pieces on the bitboard
+    - The base class 'Pieces' holds all the base logic
+    - and data that extend to all the other pieces
+    -
+    - those then get displayed and have their data mutated
+    - through the separate logic functions
+
+class Pieces {
+public:
+    Bitboard positions;
+    bool attacked, pinned, checking;
+    const int value;
+    const char * moves;
+    const int directions[];
+}
+
+class Rook : Pieces {
+
+}
+**/
