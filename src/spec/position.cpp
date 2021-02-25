4eb0406ac3e68f1227771f7303a0bd1ef7fcf010
izzy 🌺🌺 <cassell.izzy@gmail.com>
2021-02-25 16:45:34 +0000

    dwaiojdaow

--- src/spec/position.cpp
+++ src/spec/position.cpp
@@ -1,9 +1,18 @@
-#include <iostream>
+#include "iostream"
+
+#include "position.hpp"
 
 using namespace std;
 
-int idk() {
+void bitscan() {
+    Bitboard test;
+
+    test.set(5,5);
+    test.board[5] = 1;
+    for (std::size_t i = 0; i < test.board.size(); ++i) {
+        std::cout << "test.board[" << i << "]: " << test.board[i] << '\n';
+    }
+
     cout << "position.cpp" << endl;
-    return 0;
 }
 
