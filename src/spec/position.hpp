27e547c9159142c563c7cb8595979a62305397a1
izzy 🌺🌺 <cassell.izzy@gmail.com>
2021-02-27 14:46:20 +0000

    fen to board

--- src/spec/position.hpp
+++ src/spec/position.hpp
@@ -11,6 +11,24 @@
  *
 **/
 
+enum BoardPositions : int {
+    A1, B1, C1, D1, E1, F1, G1, H1,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A8, B8, C8, D8, E8, F8, G8, H8,
+    PL = 64,
+    PF = 0
+};
+
+enum Pieces : char {
+    p, n, b, r, q, k,
+    P, N, B, R, Q, K,
+};
+
 namespace Position {
     void set(std::string FEN, std::string play);
 }
@@ -18,8 +36,8 @@ namespace Position {
 class Bitboard {
 private:
     void initalise ();
-public:
     std::bitset<64> board;
+public:
     void set(int x, int y, bool n){
         assert( 0 <= x && x <= 7);
         assert( 0 <= y && y <=7);
@@ -30,6 +48,7 @@ public:
     auto outerIterator (int x, int y);
 };
 
+/**
 struct Pieces
 {   // Define piece values & such here
 
@@ -40,7 +59,6 @@ struct Pieces
 
 };
 
-/**
 - Another idea on how to implement pieces on the bitboard
     - The base class 'Pieces' holds all the base logic
     - and data that extend to all the other pieces
