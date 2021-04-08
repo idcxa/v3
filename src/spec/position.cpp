9aa4e688c5ab44977827fe71d56c814f0b6190e2
izzy 🌺🌺 <cassell.izzy@gmail.com>
2021-04-08 02:06:47 +0100

    stuffs

--- src/spec/position.cpp
+++ src/spec/position.cpp
@@ -7,22 +7,26 @@
 
 using namespace std;
 
+BoardData board;
+
 void Position::set(string FEN, string movelist) {
 
     char token;
     istringstream ss(FEN);
 
-    char board[PL];
+    board.checking.set(5,5, true);
+
+    board.colour = 'b';
 
     ss >> std::noskipws;
 
-    /* loop through FEN */
+    /* piece positions */
     int i = 0;
     string p = " pnbrqk PNBRQK ";
     while ((ss >> token) && !isspace(token)) {
         if (isdigit(token)) {
             for (int j = 0; j <= 7; j++) {
-                board[j+i] = '.';
+                board.piecepositions[j+i] = '1';
             }
             i += (token - 48);
         }
@@ -32,18 +36,70 @@ void Position::set(string FEN, string movelist) {
         }
 
         else if (p.find(token)) {
-            board[i] = token;
+            board.piecepositions[i] = token;
             i++;
         }
-
     }
 
-    puts("\n");
+    /* side to move */
+    ss >> token;
+    board.colour = token;
+    ss >> token;
+
+    /* castling */
+    while ((ss >> token) && !isspace(token)) {
+        if (token == 'K') {
+            board.castling.wk = true;
+        } else if (token == 'Q') {
+            board.castling.wq = true;
+        } else if (token == 'k') {
+            board.castling.bk = true;
+        } else if (token == 'q') {
+            board.castling.bq = true;
+        }
+    }
+
+    /* enpassant */
+    while ((ss >> token) && !isspace(token)) {
+        if (token == '-') {
+            board.enpassant = "";
+        } else {
+            board.enpassant.push_back(token);
+        }
+    }
+
+    /* halfmove */
+    ss >> token;
+    board.ply = token - 48;
+    ss >> token;
+
+    /* fullmove */
+    ss >> token;
+    board.movenumber = token - 48;
+
+
+    //ss >> token;
+    //cout << token;
+
+    /* print board */
+    if (board.colour == 'w')
+        printf("\nWhite to move\n");
+    else
+        printf("\nBlack to move\n");
+
+    printf("===============\n");
     for (int i = 0; i <= 7; i++) {
         for (int j = 0; j <= 7; j++) {
-            printf("%c ", board[i*8 + j]);
+            if (board.piecepositions[i*8 + j] == '1')
+                printf(". ");
+            else
+                printf("%c ", board.piecepositions[i*8 + j]);
         }
         printf("\n");
     }
+    printf("===============\n");
 }
 
+string Position::bestmove() {
+    return "bestmove idk pondering cock";
+}
--- src/spec/position.hpp
+++ src/spec/position.hpp
@@ -2,14 +2,7 @@
 #include "bitset"
 #include "cassert"
 #include "cstdint"
-
-/**	! Stuff we need	**
- *
- * 		checked
- *		attack-threat
- *		bitboards
- *
-**/
+#include "string"
 
 enum BoardPositions : int {
     A1, B1, C1, D1, E1, F1, G1, H1,
@@ -31,6 +24,7 @@ enum Pieces : char {
 
 namespace Position {
     void set(std::string FEN, std::string play);
+    std::string bestmove();
 }
 
 class Bitboard {
@@ -46,6 +40,26 @@ public:
     auto output (int x, int y);
     auto innerIterator (int x, int y);
     auto outerIterator (int x, int y);
+};
+
+struct Castling {
+    bool wk;
+    bool wq;
+    bool bk;
+    bool bq;
+};
+
+struct BoardData {
+    char piecepositions[64];
+    char colour;
+    Castling castling;
+    std::string enpassant = "";
+    int ply = 0;
+    int movenumber = 0;
+
+    Bitboard checking;
+    Bitboard attacked;
+    Bitboard pinned;
 };
 
 /**
